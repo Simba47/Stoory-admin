dcb91153adcd2d51bd996d1de3defc6905169cf7
Narasimha Mallela <141912420+Simba47@users.noreply.github.com>
2026-01-21 16:51:26 +0530

    buliding admin page

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require(".utils/db"); // your PostgreSQL connection
+const pool = require("./utils/db"); // your PostgreSQL connection
 
 // GET all applications (Admin)
 router.get("/applications", async (req, res) => {
